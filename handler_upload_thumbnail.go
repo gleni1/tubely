911f49bbc8e5ea63c3dc19f1de5cb69270969e87
gleni1 <glenipoleshi@gmail.com>
2025-01-02 18:36:52 -0500

    Implemented video upload function that handles multipart form uploads of thumbnails, stores data in memory, and updates related video thumbnail data in the database.

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+  "io"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -31,7 +32,51 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+  const maxMemory = 10 << 20
+  r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+  file, header, err := r.FormFile("thumbnail")
+  if err != nil {
+    respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+    return
+  }
+  defer file.Close()
+
+  mediaType := header.Header["Content-Type"][0]
+
+  var vidData []byte
+  vidData, err = io.ReadAll(file)
+  if err != nil {
+    respondWithError(w, http.StatusBadRequest, "Unable to read image data into slice", err)
+    return
+  }
+
+  video, err := cfg.db.GetVideo(videoID)
+  if err != nil {
+    respondWithError(w, http.StatusBadRequest, "Unable to get videos from db", err)
+    return
+  }
+
+  if video.UserID != userID {
+    respondWithError(w, http.StatusUnauthorized, "Current user is not the author of the videos", nil)
+    return
+  }
+
+  thmb := thumbnail{
+    data:      vidData,
+    mediaType: mediaType,
+  }
+
+  videoThumbnails[video.ID] = thmb
+
+  newThumbnail := fmt.Sprintf("http://localhost:%d/api/thumbnails/%d", cfg.port, video.ID)
+  video.ThumbnailURL = &newThumbnail
+
+  err = cfg.db.UpdateVideo(video)
+  if err != nil {
+    respondWithError(w, http.StatusBadRequest, "Cannot update video", err)
+    return
+  }
+
+	respondWithJSON(w, http.StatusOK, video)
 }
